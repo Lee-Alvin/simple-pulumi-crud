50f92d017ba78adb93d1990e5b2ae0daeef6ac95
Alvin Lee <alvinlee4197@gmail.com>
2023-02-01 21:32:39 -0500

    New linter rules

--- src/blueprints/user/__init__.py
+++ src/blueprints/user/__init__.py
@@ -1,20 +1,22 @@
 from flask import Blueprint, request
 from dynamo_util import create_user, get_user, update_user, delete_user, index_query
 
-blueprint = Blueprint('api', __name__, url_prefix='/simple-crud-api')
+blueprint = Blueprint("api", __name__, url_prefix="/simple-crud-api")
 
-@blueprint.route('/user', methods=['POST', 'GET', 'DELETE', 'PUT'])
+
+@blueprint.route("/user", methods=["POST", "GET", "DELETE", "PUT"])
 def user():
     match request.method:
-        case 'GET':
+        case "GET":
             return get_user(request.json)
-        case 'POST':
+        case "POST":
             return create_user(request.json)
-        case 'DELETE':
+        case "DELETE":
             return delete_user(request.json)
-        case 'PUT':
+        case "PUT":
             return update_user(request.json)
 
-@blueprint.route('/user/index', methods=['GET'])
+
+@blueprint.route("/user/index", methods=["GET"])
 def index():
     return index_query(request.json)
